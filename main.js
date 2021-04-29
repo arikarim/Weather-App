a769e590b7e5417e1900a98d46fb85b3e5770cba
arikarim <arikarim146@gmail.com>
2021-04-29 22:03:41 +0300

    Get user input

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 const form = document.getElementById('form')
 const input = document.getElementById('input')
+const submit = document.getElementById('submit')
+
+  function getInput() {
+  submit.addEventListener('click', e => {
+    e.preventDefault();
+    const location = input.value;
+    input.value = '';
+    return location
+  })
+}
+
 
 async function getData(url) {
   const response = await fetch(url, {mode: 'cors'})
   const data = response.json()
   console.log(data)
 }
+function getWeather() {
+  const location = getInput()
+getData(`http://api.weatherapi.com/v1/forecast.json?key=59fc7f51da5e46f88ae184111212904&q=paris`)
 
-getData('http://api.weatherapi.com/v1/forecast.json?key=59fc7f51da5e46f88ae184111212904&q=paris')
+}
 
+getWeather()
