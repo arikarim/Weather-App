c84dc53ae6c72907ce574a2a30ee164dd83e6124
arikarim <arikarim146@gmail.com>
2021-04-29 21:45:35 +0300

    Add async function

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+async function getData(url) {
+  const response = await fetch(url, {mode: 'cors'})
+  const data = response.json()
+  console.log(data)
+}
+
+getData('http://api.weatherapi.com/v1/forecast.json?key=59fc7f51da5e46f88ae184111212904&q=paris')
+
