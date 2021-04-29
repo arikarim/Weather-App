22d75685f95b565b443064cc5e83cf81754ec453
arikarim <arikarim146@gmail.com>
2021-04-29 22:56:23 +0300

    Add searchable locations

--- main.js
+++ main.js
@@ -2,25 +2,18 @@ const form = document.getElementById('form')
 const input = document.getElementById('input')
 const submit = document.getElementById('submit')
 
-  function getInput() {
-  submit.addEventListener('click', e => {
-    e.preventDefault();
-    const location = input.value;
-    input.value = '';
-    return location
-  })
-}
+    submit.addEventListener('click', e => {
+      e.preventDefault();
+      const location = input.value;
+      input.value = '';
+      getData(location)
+    })
 
 
-async function getData(url) {
-  const response = await fetch(url, {mode: 'cors'})
+
+async function getData(location) {
+  const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=59fc7f51da5e46f88ae184111212904&q=${location}`, {mode: 'cors'})
   const data = response.json()
   console.log(data)
 }
-function getWeather() {
-  const location = getInput()
-getData(`http://api.weatherapi.com/v1/forecast.json?key=59fc7f51da5e46f88ae184111212904&q=paris`)
 
-}
-
-getWeather()
