3134443fc190bb20216caababad8d2a4db755570
arikarim <arikarim146@gmail.com>
2021-04-30 04:02:15 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
-import _ from 'lodash';
 import './style.css';
 import Rainy from './img/rainy.jpg';
 import Snowy from './img/snowy.jpg';
 import Cloudy from './img/cloudy.jpg';
 import Sunny from './img/sunny.jpg';
 
-const form = document.getElementById('form')
-const input = document.getElementById('input')
-const submit = document.getElementById('submit')
-const contentDiv = document.getElementById('content')
-const body = document.getElementById('body')
-  const image = new Image()
-  image.src = Rainy
-  body.style.backgroundImage = `url(${Rainy})`
-
-    submit.addEventListener('click', e => {
-      e.preventDefault();
-      const location = input.value;
-      input.value = '';
-      getData(location)
-    })
+const input = document.getElementById('input');
+const submit = document.getElementById('submit');
+const contentDiv = document.getElementById('content');
+const body = document.getElementById('body');
+const image = new Image();
+image.src = Rainy;
+body.style.backgroundImage = `url(${Rainy})`;
 
 function deleteEl(element) {
   while (element.firstChild) {
@@ -27,69 +18,52 @@ function deleteEl(element) {
   }
 }
 
-async function getData(location) {
-  const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=59fc7f51da5e46f88ae184111212904&q=${location}`, {mode: 'cors'})
-  const data = await response.json()
-  let test = processData(data)
-  displayData(test)
-
-  console.log(test.location)
-  
-}
-
-function displayData(data)  {
-  deleteEl(contentDiv)
+function displayData(data) {
+  deleteEl(contentDiv);
   // condition of weather
-  const condition = document.createElement('h4')
-  condition.classList.add('text-light')
-  condition.textContent = data.condition
-  contentDiv.appendChild(condition)
-
-  // const image = new Image()
-  // image.src = Rainy
-  // body.appendChild(image)
+  const condition = document.createElement('h4');
+  condition.classList.add('text-light');
+  condition.textContent = data.condition;
+  contentDiv.appendChild(condition);
 
   if (data.condition.includes('Clear') || data.condition.includes('Sunny')) {
-    body.style.backgroundImage = `url(${Sunny})`
+    body.style.backgroundImage = `url(${Sunny})`;
   } else if (data.condition.includes('rain')) {
-    body.style.backgroundImage = `url(${Rainy})`
-  } else if ( data.condition.includes('snow')) {
-    body.style.backgroundImage = `url(${Snowy})`
-  } else if ( data.condition.includes('cloudy')) {
-    body.style.backgroundImage = `url(${Cloudy})`
+    body.style.backgroundImage = `url(${Rainy})`;
+  } else if (data.condition.includes('snow')) {
+    body.style.backgroundImage = `url(${Snowy})`;
+  } else if (data.condition.includes('cloudy')) {
+    body.style.backgroundImage = `url(${Cloudy})`;
   }
 
   // name of city
-  const newH = document.createElement('h1')
-  newH.classList.add('text-light')
-  newH.textContent = data.location
-  contentDiv.appendChild(newH)
+  const newH = document.createElement('h1');
+  newH.classList.add('text-light');
+  newH.textContent = data.location;
+  contentDiv.appendChild(newH);
 
   // Tempreature
-  const div = document.createElement('div')
-  div.classList.add('d-flex')
-  div.classList.add('temp-div')
+  const div = document.createElement('div');
+  div.classList.add('d-flex');
+  div.classList.add('temp-div');
 
-  const temp = document.createElement('span')
-  temp.classList.add('text-light')
-  temp.classList.add('temp-number')
-  temp.textContent = data.currentTemp.c
+  const temp = document.createElement('span');
+  temp.classList.add('text-light');
+  temp.classList.add('temp-number');
+  temp.textContent = data.currentTemp.c;
 
-  div.appendChild(temp)
+  div.appendChild(temp);
 
-  const div2 = document.createElement('div')
-  div2.classList.add('div2')
+  const div2 = document.createElement('div');
+  div2.classList.add('div2');
 
   div2.innerHTML = `
   <p class="my-3 text-light">FEELS LIKE: ${data.feelsLike.c}</p>
   <p class="my-3 text-light">WIND: ${data.wind} MPH</p>
   <p class="my-3 text-light">HMIDITY: ${data.humidity}%</p>`;
 
-
-div.appendChild(div2)
-  contentDiv.appendChild(div)
-
-
+  div.appendChild(div2);
+  contentDiv.appendChild(div);
 }
 
 function processData(weatherData) {
@@ -106,12 +80,22 @@ function processData(weatherData) {
     },
     wind: Math.round(weatherData.current.wind_mph),
     humidity: weatherData.current.humidity,
-    location: weatherData['location'].name.toUpperCase(),
+    location: weatherData.location.name.toUpperCase(),
   };
 
-  return myData
-
+  return myData;
 }
 
+async function getData(location) {
+  const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=59fc7f51da5e46f88ae184111212904&q=${location}`, { mode: 'cors' });
+  const data = await response.json();
+  const test = processData(data);
+  displayData(test);
+}
 
-
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  const location = input.value;
+  input.value = '';
+  getData(location);
+});
